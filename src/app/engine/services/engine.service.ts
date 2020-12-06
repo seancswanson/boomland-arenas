0c208e6d2dd6d85dfd523685f05678dda1f69052
Sean Swanson <sean-swan@hotmail.com>
2020-12-05 19:08:36 -0800

    Now loading the 3d engine for the scene.

--- src/app/engine/services/engine.service.ts
+++ src/app/engine/services/engine.service.ts
@@ -1,18 +1,41 @@
 import { ElementRef, Injectable } from "@angular/core";
-import * as BABYLON from "babylonjs";
+import {
+  Engine,
+  FreeCamera,
+  Scene,
+  Light,
+  Mesh,
+  Color3,
+  Color4,
+  Vector3,
+  HemisphericLight,
+  StandardMaterial,
+  Texture,
+  DynamicTexture,
+  Camera,
+} from "babylonjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class EngineService {
   private canvas: HTMLCanvasElement;
+  private engine: Engine;
+
   constructor() {}
 
   /**
-   * createScene
+   * createScene creates and returns the scene
    */
   public createScene(canvas: ElementRef<HTMLCanvasElement>) {
+    // Get the canvas DOM element
     this.canvas = canvas.nativeElement;
-    console.log("createScene", this.canvas);
+    // Load the 3d engine
+    this.engine = new Engine(this.canvas, true, {
+      preserveDrawingBuffer: true,
+      stencil: true,
+    });
+    // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
+    console.log("createScene", this.canvas, this.engine);
   }
 }
